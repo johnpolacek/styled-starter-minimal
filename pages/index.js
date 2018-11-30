ef55fd6248b6eb0fc8e0fa23dba05ec103c60ab8
johnpolacek <johnpolacek@gmail.com>
2018-11-29 19:31:57 -0600

    add document for serving required styles on first render

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import Head from './Head'
 import Body from './Body'
 import { H1 } from 'styled-system-html'
@@ -9,7 +8,7 @@ export default class extends React.Component {
 		return <div>
 		    <Head />
 		    <Body>
-		    	<H1 fontSize={6} textAlign="center" p={6} color="primary">YO!</H1>
+	    		<H1 fontSize={6} textAlign="center" p={6} color="primary">hey yo</H1>
 		    </Body>
 	    </div>
 	}
